bc3ddfdcfd610af042e82a592e086526b7ac5af8
Cameron Lakenen <camupod@gmail.com>
2013-12-11 17:56:08 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Static file processor plugin for [Hexo](http://zespia.tw/hexo)
+=================================
+
+Simply copies any files from `source/_static/` directly into the `public/` directory during static file generation.
---
+++ index.js
@@ -0,0 +1,33 @@
+var processor = hexo.extend.processor;
+
+var fs = require('graceful-fs'),
+  pathFn = require('path');
+
+processor.register('_static/*path', function(data, callback){
+  var Asset = hexo.model('Asset');
+  var path = data.path,
+    src = pathFn.join('source', path),
+    doc = Asset.findOne({source: src});
+
+  if (data.type === 'delete' && doc){
+    hexo.route.remove(path);
+    doc.remove();
+
+    return callback();
+  }
+
+  Asset.checkModified(src, function(err, modified){
+    if (err) {
+      return callback(err);
+    }
+
+    var content = function(fn){
+      fn(null, fs.createReadStream(data.source));
+    };
+
+    content.modified = modified;
+
+    hexo.route.set(data.params.path, content);
+    callback();
+  });
+});
