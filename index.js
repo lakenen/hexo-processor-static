2552a8b544b1f555ec1a0a4d435ff596e7e5a04b
Ray Villalobos <ray@planetoftheweb.com>
2018-01-12 17:25:35 -0500

    removed test output statements

--- index.js
+++ index.js
@@ -3,40 +3,37 @@ var copy_src, copy_dst;
 
 var fs = require('hexo-fs');
 var pathFn = require('path');
+var config = hexo.config;
 
-if (hexo.config.copy_md_src) {
-    copy_src = hexo.config.copy_md_src;
+if (config.copy_src) {
+    copy_src = config.copy_src;
 } else {
     copy_src = '_static';
 }
 
-if (hexo.config.copy_dst) {
-    copy_dst = hexo.config.copy_md_dst;
+if (config.copy_dst) {
+    copy_dst = config.copy_dst;
 } else {
-    copy_src = '';
+    copy_dst = '';
 }
 
-var copy_dst = hexo.config.copy_md_dst;
-
-if ( fs.exists(copy_src)) {
-    processor.register( copy_src + '/*path', function static_processor(data) {
-        var Asset = hexo.model('Asset');
-        var path = data.path,
-        src = pathFn.join('source', path),
-        doc = Asset.findOne({source: src});
-        if (data.type === 'delete'){
-            if (doc) {
-                return doc.remove();
-            }
-            return;
+processor.register( copy_src + '/*path', function static_processor(data) {
+    var Asset = hexo.model('Asset');
+    var path = data.path,
+    src = pathFn.join('source', path),
+    doc = Asset.findOne({source: src});
+    if (data.type === 'delete'){
+        if (doc) {
+            return doc.remove();
         }
+        return;
+    }
 
-        var id = data.source.substring(hexo.base_dir.length).replace(/\\/g, '/');
-        return Asset.save({
-            _id: id,
-            path: path.replace( copy_src, copy_dst),
-            modified: data.type !== 'skip',
-            renderable: false
-        })
-      });
-}
+    var id = data.source.substring(hexo.base_dir.length).replace(/\\/g, '/');
+    return Asset.save({
+        _id: id,
+        path: path.replace( copy_src, copy_dst),
+        modified: data.type !== 'skip',
+        renderable: false
+    })
+});
